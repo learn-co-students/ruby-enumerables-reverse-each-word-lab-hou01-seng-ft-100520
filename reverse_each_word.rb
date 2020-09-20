19f23225baec56da0d5d3f1d670dfef7c4619e71
Jordan Dains <dains.jordan2@gmail.com>
2020-09-20 18:16:04 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,11 @@
 def reverse_each_word (strings)
   array = strings.split();
   new_array = []
+  
   for word in array do
     new_array.push(word.split("").join.reverse)
   end
-  new_array = new_array.collect{|word|
-    word = word
-  }
+  new_array = new_array.collect{|word| word = word}
   
 p new_array.join(" ")
 end
